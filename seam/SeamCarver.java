242579f038cb5f2c69f90d04612d7478e5b22621
Meg Dahlgren <meg.dahlgren@gmail.com>
2019-08-26 18:35:03 -0400

    Pre code review commit

--- seam/SeamCarver.java
+++ seam/SeamCarver.java
@@ -4,6 +4,7 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.IndexMinPQ;
 import edu.princeton.cs.algs4.Picture;
 import edu.princeton.cs.algs4.StdOut;
 
@@ -13,6 +14,7 @@ public class SeamCarver {
 
     private Picture picture;
     private double[][] energy;
+    private int[][] color;
     private int height;
     private int width;
 
@@ -24,13 +26,18 @@ public class SeamCarver {
         this.height = this.picture.height();
         this.width = this.picture.width();
         this.energy = new double[height][width];
+        this.color = new int[height][width];
+        for (int row = 0; row < this.height; row++) {
+            for (int col = 0; col < this.width; col++) {
+                color[row][col] = picture.getRGB(col, row);
+            }
+        }
         calculateEnergyMatrix();
     }
 
     private void calculateEnergyMatrix() {
         for (int row = 0; row < this.height; row++) {
             for (int col = 0; col < this.width; col++) {
-                // StdOut.println("col: " + col + " row " + row);
                 energy[row][col] = energy(col, row);
             }
         }
@@ -48,16 +55,21 @@ public class SeamCarver {
 
     // height of current picture
     public int height() {
-        return this.picture.height();
+        return this.height;
     }
 
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
         if (x == 0 || x == width() - 1 || y == 0 || y == height() - 1) return 1000;
-        int rgbXPlusOne = picture.getRGB(x + 1, y);
-        int rgbXMinusOne = picture.getRGB(x - 1, y);
-        int rgbYPlusOne = picture.getRGB(x, y + 1);
-        int rgbYMinusOne = picture.getRGB(x, y - 1);
+        // int rgbXPlusOne = picture.getRGB(x + 1, y);
+        // int rgbXMinusOne = picture.getRGB(x - 1, y);
+        // int rgbYPlusOne = picture.getRGB(x, y + 1);
+        // int rgbYMinusOne = picture.getRGB(x, y - 1);
+        // StdOut.println("energy: x " + x + " y " + y + " height " + height() + " width " + width());
+        int rgbXPlusOne = this.color[y][x + 1];
+        int rgbXMinusOne = this.color[y][x - 1];
+        int rgbYPlusOne = this.color[y + 1][x];
+        int rgbYMinusOne = this.color[y - 1][x];
         return Math.sqrt(gradientSquared(rgbXPlusOne, rgbXMinusOne) + gradientSquared(rgbYPlusOne,
                                                                                       rgbYMinusOne));
     }
@@ -79,21 +91,40 @@ public class SeamCarver {
         return (rgb >> 0) & 0xFF;
     }
 
-    private double[][] transposeMatrix() {
-        int newWidth = this.height;
-        int newHeight = this.width;
+    private double[][] transposeEnergyMatrix(double[][] energyMatrix, int originalHeight,
+                                             int originalWidth) {
+        int newWidth = originalHeight;
+        int newHeight = originalWidth;
         double[][] transposedMatrix = new double[newHeight][newWidth];
         for (int row = 0; row < newHeight; row++) {
             for (int col = 0; col < newWidth; col++) {
-                transposedMatrix[row][col] = this.energy[col][row];
+                transposedMatrix[row][col] = energyMatrix[col][row];
             }
         }
         return transposedMatrix;
     }
 
+    private int[][] transposeColorMatrix(int[][] colorMatrix, int originalHeight,
+                                         int originalWidth) {
+        StdOut.println("input matrix height " + colorMatrix.length + " input width "
+                               + colorMatrix[0].length + " originalHeight " + originalHeight
+                               + " originalWidth " + originalWidth);
+        int newWidth = originalHeight;
+        int newHeight = originalWidth;
+        int[][] transposedMatrix = new int[newHeight][newWidth];
+        for (int row = 0; row < newHeight; row++) {
+            for (int col = 0; col < newWidth; col++) {
+                transposedMatrix[row][col] = colorMatrix[col][row];
+            }
+        }
+        return transposedMatrix;
+    }
+
+
     // sequence of indices for horizontal seam
     public int[] findHorizontalSeam() {
-        double[][] transposedEnergyMatrix = transposeMatrix();
+        double[][] transposedEnergyMatrix = transposeEnergyMatrix(this.energy, this.height,
+                                                                  this.width);
         return findSeamHelper(transposedEnergyMatrix, this.height, this.width);
     }
 
@@ -102,41 +133,14 @@ public class SeamCarver {
         return findSeamHelper(this.energy, this.width, this.height);
     }
 
+
     private int[] findSeamHelper(double[][] matrix, int width, int height) {
-        double minSeamEnergy = Double.POSITIVE_INFINITY;
-        int[] seam = new int[height];
-        for (int col = 0; col < width; col++) {
-            int[] newSeam = new int[height];
-            newSeam[0] = col;
-            double previousSeamEnergy = matrix[0][col];
-            double seamEnergy = previousSeamEnergy;
-            int row = 1;
-            int pathFrom = col;
-            while (row < height) {
-                ArrayList<Integer> reachableCols = reachableColumns(pathFrom, width);
-                seamEnergy = previousSeamEnergy + matrix[row][reachableCols.get(0)];
-                newSeam[row] = reachableCols.get(0);
-                pathFrom = reachableCols.get(0);
-                for (int reachableCol : reachableCols) {
-                    double pathCandidateEnergy = previousSeamEnergy + matrix[row][reachableCol];
-                    if (pathCandidateEnergy < seamEnergy) {
-                        seamEnergy = previousSeamEnergy + matrix[row][reachableCol];
-                        newSeam[row] = reachableCol;
-                        pathFrom = reachableCol;
-                    }
-                }
-                row++;
-                previousSeamEnergy = seamEnergy;
-            }
-            if (seamEnergy < minSeamEnergy) {
-                minSeamEnergy = seamEnergy;
-                seam = newSeam;
-            }
-        }
+        Dijkstra dijkstra = new Dijkstra(matrix, width, height);
+        int[] seam = dijkstra.shortestPath();
         return seam;
     }
 
-    private ArrayList<Integer> reachableColumns(int col, int width) {
+    private ArrayList<Integer> adjacent(int col, int width) {
         if (col < 0) throw new IllegalArgumentException("column cannot be less than zero");
         ArrayList<Integer> reachable = new ArrayList<>();
         if (col - 1 >= 0) reachable.add(col - 1);
@@ -145,11 +149,70 @@ public class SeamCarver {
         return reachable;
     }
 
+    private int[][] shiftColorMatrix(int[][] matrix, int[] seam, int width, int height) {
+        int[][] tempColor = new int[height][width];
+        for (int row = 0; row < matrix.length; row++) {
+            int[] pixelRow = matrix[row];
+            int elim = seam[row];
+            for (int col = 0; col < pixelRow.length; col++) {
+                if (col < elim) tempColor[row][col] = matrix[row][col];
+                else if (col > elim) tempColor[row][col - 1] = matrix[row][col];
+            }
+        }
+        return tempColor;
+    }
+
     // remove horizontal seam from current picture
-    // public void removeHorizontalSeam(int[] seam)
+    public void removeHorizontalSeam(int[] seam) {
+        int newHeight = this.height - 1;
+        int[][] colorT = transposeColorMatrix(this.color, this.height, this.width);
+        int[][] tempColorT = shiftColorMatrix(colorT, seam, newHeight, this.width);
+        int[][] colorTT = transposeColorMatrix(tempColorT, this.width, newHeight);
+        this.color = colorTT;
+        double[][] energyT = transposeEnergyMatrix(this.energy, this.height, this.width);
+        double[][] tempEnergyT = seamRemovalHelper(energyT, seam, newHeight, this.width);
+        double[][] energyTT = transposeEnergyMatrix(tempEnergyT, this.width, newHeight);
+        this.energy = energyTT;
+        this.height = newHeight;
+    }
 
     // remove vertical seam from current picture
-    // public void removeVerticalSeam(int[] seam)
+    public void removeVerticalSeam(int[] seam) {
+        int newWidth = this.width - 1;
+        this.width = newWidth;
+        int[][] tempColor = shiftColorMatrix(this.color, seam, this.width, this.height);
+        this.color = tempColor;
+        double[][] tempEnergy = seamRemovalHelper(this.energy, seam, this.width, this.height);
+        this.energy = tempEnergy;
+    }
+
+    private double[][] seamRemovalHelper(double[][] energyMatrix, int[] seam, int width,
+                                         int height) {
+        double[][] tempEnergy = new double[height][width];
+        for (int x = 0; x < energyMatrix.length; x++) {
+            double[] energyRow = energyMatrix[x];
+            int elim = seam[x];
+            for (int y = 0; y < energyRow.length; y++) {
+                if (y < elim) {
+                    if (y == elim - 1) {
+                        tempEnergy[x][y] = energy(y, x);
+                    }
+                    else {
+                        tempEnergy[x][y] = energyMatrix[x][y];
+                    }
+                }
+                else if (y > elim) {
+                    if (y == elim + 1) {
+                        tempEnergy[x][y - 1] = energy(y - 1, x);
+                    }
+                    else {
+                        tempEnergy[x][y - 1] = energyMatrix[x][y];
+                    }
+                }
+            }
+        }
+        return tempEnergy;
+    }
 
     //  unit testing (optional)
     public static void main(String[] args) {
@@ -179,7 +242,7 @@ public class SeamCarver {
 
         StdOut.printf("Printing transposed energy matrix.\n");
 
-        double[][] transposedEnergy = sc.transposeMatrix();
+        double[][] transposedEnergy = sc.transposeEnergyMatrix(sc.energy, sc.height, sc.width);
 
         for (int row = 0; row < sc.width(); row++) {
             for (int col = 0; col < sc.height(); col++)
@@ -189,4 +252,150 @@ public class SeamCarver {
         }
 
     }
+
+
+    private class Dijkstra {
+
+        private Edge[] edgeTo;
+        private double[] distTo;
+        private IndexMinPQ<Double> pq;
+        private int top = 0;
+        private int bottom;
+        private int width;
+        private int height;
+        private double[][] energy;
+        private int[] shortestPath;
+
+        public Dijkstra(double[][] matrix, int width, int height) {
+            this.energy = matrix;
+            this.edgeTo = new Edge[height + 2];
+            this.distTo = new double[height + 2];
+            this.pq = new IndexMinPQ<Double>((width * height) + 2);
+            this.bottom = height + 1;
+            this.width = width;
+            this.height = height;
+            this.shortestPath = new int[this.height];
+
+            for (int v = 0; v < bottom; v++) {
+                distTo[v] = Double.POSITIVE_INFINITY;
+            }
+            distTo[top] = 0.0;
+
+            // initialize row
+            int nextRow = 0;
+
+            pq.insert(top, 0.0);
+            while (!pq.isEmpty()) {
+                int row = pq.delMin();
+                int col = edgeTo[row] != null ? edgeTo[row].w().col() : 0;
+                for (Edge e : adj(new Vertex(nextRow, col))) {
+                    StdOut.println(
+                            "next adjacent edge from: " + e.v().row() + " " + e.v().col() + " to "
+                                    + e.w().row() + " " + e.w().col());
+                    relax(e);
+                }
+                nextRow++;
+            }
+
+        }
+
+        public int[] shortestPath() {
+            for (int row = 1; row < this.height; row++) {
+                StdOut.println("row " + row + " height " + this.height + " edgeTo length "
+                                       + this.edgeTo.length + " shortestPath length "
+                                       + this.shortestPath.length);
+                StdOut.println("this.edgeTo[row] " + this.edgeTo[row]);
+                this.shortestPath[row] = this.edgeTo[row].w().col();
+            }
+            return this.shortestPath;
+        }
+
+        // return adjacent columns
+        public ArrayList<Edge> adj(Vertex v) {
+            int rowFrom = v.row();
+            int colFrom = v.col();
+            int row = rowFrom + 1;
+            ArrayList<Edge> adjacent = new ArrayList<>();
+            if (rowFrom < -1 || rowFrom >= this.bottom) return adjacent;
+            else if (rowFrom == -1) {
+                for (int i = 0; i < this.width; i++) {
+                    adjacent.add(new Edge(v, new Vertex(row, i)));
+                }
+                return adjacent;
+            }
+            else if (row == this.height) {
+                adjacent.add(new Edge(v, new Vertex(this.bottom, 0)));
+                return adjacent;
+            }
+            else {
+                if (colFrom - 1 >= 0) adjacent.add(new Edge(v, new Vertex(row, colFrom - 1)));
+                adjacent.add(new Edge(v, new Vertex(row, colFrom)));
+                if (colFrom + 1 < width) adjacent.add(new Edge(v, new Vertex(row, colFrom + 1)));
+            }
+            return adjacent;
+        }
+
+        public void relax(Edge e) {
+            Vertex v = e.v();
+            Vertex w = e.w();
+            if (distTo[w.row()] > distTo[v.row()] + e.weight()) {
+                StdOut.println(
+                        "relaxing w.row() " + w.row() + " distTo[w.row()] " + distTo[w.row()]);
+                distTo[w.row()] = distTo[v.row()] + e.weight();
+                edgeTo[w.row()] = e;
+                if (pq.contains(w.row())) pq.decreaseKey(w.row(), distTo[w.row()]);
+                else pq.insert(w.row(), distTo[w.row()]);
+            }
+        }
+
+        private class Edge {
+            private Vertex v; // from
+            private Vertex w; // to
+            private double weight;
+
+            public Edge(Vertex from, Vertex to) {
+                this.v = from;
+                this.w = to;
+                if (this.w.row() == Dijkstra.this.bottom) {
+                    this.weight = 0.0;
+                }
+                else {
+                    this.weight = Dijkstra.this.energy[this.w.row()][this.w.col()];
+                }
+            }
+
+            public Vertex v() {
+                return this.v;
+            }
+
+            public Vertex w() {
+                return this.w;
+            }
+
+            public double weight() {
+                return this.weight;
+            }
+        }
+
+        private class Vertex {
+            private int row;
+            private int col;
+
+            public Vertex(int row, int col) {
+                this.row = row;
+                this.col = col;
+            }
+
+            public int row() {
+                return this.row;
+            }
+
+            public int col() {
+                return this.col;
+            }
+
+        }
+
+    }
+
 }
