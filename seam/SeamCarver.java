4cb17b985132ce9fe60f832a5efd49380064e5ab
Meg Dahlgren <meg.dahlgren@gmail.com>
2019-09-10 14:22:34 -0400

    Refactor seam carving

--- seam/SeamCarver.java
+++ seam/SeamCarver.java
@@ -15,8 +15,6 @@ public class SeamCarver {
     private Picture picture;
     private double[][] energy;
     private int[][] color;
-    private int height;
-    private int width;
     private SeamCarverHelper verticalCarver, horizontalCarver;
 
     // create a seam carver object based on the given picture
@@ -25,25 +23,25 @@ public class SeamCarver {
                 "must provide a Picture object to the SeamCarver constructor");
         this.picture = new Picture(picture);
 
-        this.height = this.picture.height();
-        this.width = this.picture.width();
-        this.energy = new double[height][width];
+        int height = this.picture.height();
+        int width = this.picture.width();
         this.color = new int[height][width];
-        for (int row = 0; row < this.height; row++) {
-            for (int col = 0; col < this.width; col++) {
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
                 color[row][col] = picture.getRGB(col, row);
             }
         }
-        calculateEnergyMatrix();
 
-        this.verticalCarver = new SeamCarverHelper(this.color, this.energy);
-        this.horizontalCarver = new SeamCarverHelper(transposeColorMatrix(this.color),
-                                                     transposeEnergyMatrix(this.energy));
+        this.verticalCarver = new SeamCarverHelper(this.color);
+        this.horizontalCarver = new SeamCarverHelper(transposeColorMatrix(this.color));
+        this.energy = verticalCarver.getEnergyMatrix();
     }
 
     private void calculateEnergyMatrix() {
-        for (int row = 0; row < this.height; row++) {
-            for (int col = 0; col < this.width; col++) {
+        int height = this.picture.height();
+        int width = this.picture.width();
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
                 energy[row][col] = energy(col, row);
             }
         }
@@ -51,11 +49,13 @@ public class SeamCarver {
 
     // current picture
     public Picture picture() {
+        int width = this.color[0].length;
+        int height = this.color.length;
         if (this.picture != null) return new Picture(this.picture);
         else {
-            this.picture = new Picture(this.width, this.height);
-            for (int col = 0; col < this.width; col++) {
-                for (int row = 0; row < this.height; row++) {
+            this.picture = new Picture(width, height);
+            for (int col = 0; col < width; col++) {
+                for (int row = 0; row < height; row++) {
                     this.picture.setRGB(col, row, this.color[row][col]);
                 }
             }
@@ -65,26 +65,17 @@ public class SeamCarver {
 
     // width of current picture
     public int width() {
-        return this.width;
+        return this.color[0].length;
     }
 
     // height of current picture
     public int height() {
-        return this.height;
+        return this.color.length;
     }
 
     // energy of pixel at column x and row y
     public double energy(int col, int row) {
-        if (col < 0 || row < 0 || col > this.width - 1 || row > this.height - 1)
-            throw new IllegalArgumentException(
-                    "x and y must be greater than 0, less than width, height respectively");
-        if (col == 0 || col == width - 1 || row == 0 || row == height - 1) return 1000;
-        int rgbXPlusOne = this.color[row][col + 1];
-        int rgbXMinusOne = this.color[row][col - 1];
-        int rgbYPlusOne = this.color[row + 1][col];
-        int rgbYMinusOne = this.color[row - 1][col];
-        return Math.sqrt(gradientSquared(rgbXPlusOne, rgbXMinusOne) + gradientSquared(rgbYPlusOne,
-                                                                                      rgbYMinusOne));
+        return this.verticalCarver.calculateEnergy(col, row);
     }
 
     private double gradientSquared(int rgb0, int rgb1) {
@@ -134,104 +125,41 @@ public class SeamCarver {
     // sequence of indices for horizontal seam
     public int[] findHorizontalSeam() {
         double[][] transposedEnergyMatrix = transposeEnergyMatrix(this.energy);
-        return findSeamHelper(transposedEnergyMatrix, this.height, this.width);
+        return this.horizontalCarver.findSeam();
     }
 
     // sequence of indices for vertical seam
     public int[] findVerticalSeam() {
-        return findSeamHelper(this.energy, this.width, this.height);
+        return this.verticalCarver.findSeam();
     }
 
-
-    private int[] findSeamHelper(double[][] matrix, int numColumns, int numRows) {
-        Dijkstra dijkstra = new Dijkstra(matrix, numColumns, numRows);
-        int[] seam = dijkstra.shortestPath();
-        return seam;
-    }
-
-    private int[][] shiftColorMatrix(int[][] matrix, int[] seam, int width, int height) {
-        int[][] tempColor = new int[height][width];
-        for (int row = 0; row < matrix.length; row++) {
-            int[] pixelRow = matrix[row];
-            int elim = seam[row];
-            for (int col = 0; col < pixelRow.length; col++) {
-                if (col < elim) tempColor[row][col] = matrix[row][col];
-                else if (col > elim) tempColor[row][col - 1] = matrix[row][col];
-            }
-        }
-        return tempColor;
-    }
 
     // remove horizontal seam from current picture
     public void removeHorizontalSeam(int[] seam) {
         if (seam == null) throw new IllegalArgumentException("must provide a seam to remove");
-        if (seam.length != this.width)
+        if (seam.length != this.horizontalCarver.numRows())
             throw new IllegalArgumentException("seam is the wrong length");
-        if (this.height <= 1)
+        if (this.horizontalCarver.numCols() <= 1)
             throw new IllegalArgumentException("cannot remove horizonal seam when width <= 1");
         this.picture = null;
-        int targetHeight = this.height - 1;
-        int[][] colorT = transposeColorMatrix(this.color);
-        int[][] tempColorT = shiftColorMatrix(colorT, seam, targetHeight, this.width);
-        int[][] colorTT = transposeColorMatrix(tempColorT);
-        double[][] energyT = transposeEnergyMatrix(this.energy);
-        this.color = tempColorT;
-        // temp:
-        this.height = this.width;
-        this.width = targetHeight;
-        double[][] tempEnergyT = seamRemovalHelper(energyT, seam);
-        this.width = this.height; // i.e., back to original width
-        this.height = targetHeight;
-        double[][] energyTT = transposeEnergyMatrix(tempEnergyT);
-        this.color = colorTT;
-        this.energy = energyTT;
+        this.horizontalCarver.removeSeam(seam);
+        this.energy = transposeEnergyMatrix(horizontalCarver.getEnergyMatrix());
+        this.color = transposeColorMatrix(horizontalCarver.getColorMatrix());
     }
 
     // remove vertical seam from current picture
     public void removeVerticalSeam(int[] seam) {
         if (seam == null) throw new IllegalArgumentException("must provide a seam to remove");
-        if (seam.length != this.height)
+        if (seam.length != this.verticalCarver.numRows())
             throw new IllegalArgumentException("seam is the wrong length");
-        if (this.width <= 1)
+        if (this.verticalCarver.numCols() <= 1)
             throw new IllegalArgumentException("cannot remove vertical seam when height <= 1");
         this.picture = null;
-        this.width--;
-        this.color = shiftColorMatrix(this.color, seam, this.width, this.height);
-        this.energy = seamRemovalHelper(this.energy, seam);
+        verticalCarver.removeSeam(seam);
+        this.energy = verticalCarver.getEnergyMatrix();
+        this.color = verticalCarver.getColorMatrix();
     }
 
-    private double[][] seamRemovalHelper(double[][] energyMatrix, int[] seam) {
-        double[][] tempEnergy = new double[this.height][this.width];
-        int previousElimCol = seam[0];
-        for (int row = 0; row < energyMatrix.length; row++) {
-            double[] energyRow = energyMatrix[row];
-            int elimCol = seam[row];
-            // if (elimCol < 0 || elimCol >= energyMatrix.length)
-            //     throw new IllegalArgumentException("invalid seam - entry out of range");
-            if (Math.abs(elimCol - previousElimCol) > 1)
-                throw new IllegalArgumentException("invalid seam");
-            for (int col = 0; col < energyRow.length; col++) {
-                if (col < elimCol) {
-                    if (col == elimCol - 1) {
-                        tempEnergy[row][col] = energy(col, row);
-                    }
-                    else {
-                        tempEnergy[row][col] = energyMatrix[row][col];
-                    }
-                }
-                else if (col > elimCol) {
-                    if (col == elimCol + 1) {
-                        tempEnergy[row][col - 1] = energy(col - 1, row);
-                    }
-                    else {
-                        tempEnergy[row][col - 1] = energyMatrix[row][col];
-                    }
-                }
-            }
-            previousElimCol = elimCol;
-        }
-        return tempEnergy;
-    }
 
     //  unit testing (optional)
     public static void main(String[] args) {
@@ -261,7 +189,7 @@ public class SeamCarver {
 
         StdOut.printf("Printing transposed energy matrix.\n");
 
-        double[][] transposedEnergy = sc.transposeEnergyMatrix(sc.energy, sc.height, sc.width);
+        double[][] transposedEnergy = sc.transposeEnergyMatrix(sc.energy);
 
         for (int row = 0; row < sc.width(); row++) {
             for (int col = 0; col < sc.height(); col++)
@@ -272,31 +200,114 @@ public class SeamCarver {
 
     }
 
-    private static class SeamCarverHelper {
-        public SeamCarverHelper(int[][] color, double[][] energy) {
+    private class SeamCarverHelper {
+        private int[][] color;
+        private double[][] energy;
 
+        public SeamCarverHelper(int[][] color) {
+            this.color = color;
+            calculateEnergyMatrix();
+        }
+
+        private void calculateEnergyMatrix() {
+            int height = numRows();
+            int width = numCols();
+            double[][] tempEnergy = new double[height][width];
+            for (int row = 0; row < height; row++) {
+                for (int col = 0; col < width; col++) {
+                    tempEnergy[row][col] = calculateEnergy(col, row);
+                }
+            }
+            this.energy = tempEnergy;
         }
 
         public int[] findSeam() {
+            Dijkstra dk = new Dijkstra(this.energy);
+            return dk.shortestPath();
+        }
+
+        // energy of pixel at column x and row y
+        public double calculateEnergy(int col, int row) {
+            int width = numCols();
+            int height = numRows();
+            if (col < 0 || row < 0 || col > width - 1 || row > height - 1)
+                throw new IllegalArgumentException(
+                        "x and y must be greater than 0, less than width, height respectively");
+            if (col == 0 || col == width - 1 || row == 0 || row == height - 1) return 1000;
+            int rgbXPlusOne = this.color[row][col + 1];
+            int rgbXMinusOne = this.color[row][col - 1];
+            int rgbYPlusOne = this.color[row + 1][col];
+            int rgbYMinusOne = this.color[row - 1][col];
+            return Math
+                    .sqrt(gradientSquared(rgbXPlusOne, rgbXMinusOne) + gradientSquared(rgbYPlusOne,
+                                                                                       rgbYMinusOne));
         }
 
         public void removeSeam(int[] seam) {
+            resizeColorMatrix(seam);
+            int height = numRows();
+            int currentWidth = numCols();
+            double[][] newEnergy = new double[height][currentWidth - 1];
+            int previousElimCol = seam[0];
+            for (int row = 0; row < height; row++) {
+                double[] energyMatrixRow = this.energy[row];
+                int elimCol = seam[row];
+                if (elimCol < 0 || elimCol > energyMatrixRow.length - 1)
+                    throw new IllegalArgumentException("invalid seam - entry out of range");
+                if (Math.abs(elimCol - previousElimCol) > 1)
+                    throw new IllegalArgumentException("invalid seam; seam pixels not contiguous");
+                for (int col = 0; col < energyMatrixRow.length; col++) {
+                    if (col < elimCol) {
+                        if (col == elimCol - 1) {
+                            newEnergy[row][col] = calculateEnergy(col, row);
+                        }
+                        else {
+                            newEnergy[row][col] = this.energy[row][col];
+                        }
+                    }
+                    else if (col > elimCol) {
+                        if (col == elimCol + 1) {
+                            newEnergy[row][col - 1] = calculateEnergy(col - 1, row);
+                        }
+                        else {
+                            newEnergy[row][col - 1] = this.energy[row][col];
+                        }
+                    }
+                }
+                previousElimCol = elimCol;
+            }
+            this.energy = newEnergy;
+        }
 
+        public void resizeColorMatrix(int[] seam) {
+            int targetHeight = numRows() - 1;
+            int width = numCols();
+            int[][] tempColor = new int[targetHeight][width];
+            for (int row = 0; row < targetHeight; row++) {
+                int[] pixelRow = this.color[row];
+                int elim = seam[row];
+                for (int col = 0; col < pixelRow.length; col++) {
+                    if (col < elim) tempColor[row][col] = this.color[row][col];
+                    else if (col > elim) tempColor[row][col - 1] = this.color[row][col];
+                }
+            }
+            this.color = tempColor;
         }
 
         public int[][] getColorMatrix() {
-            // needed because the SeamCarverHelper won't
+            return this.color;
         }
 
         public double[][] getEnergyMatrix() {
-
+            return this.energy;
         }
 
         private int numRows() {
+            return this.color.length;
         }
 
         private int numCols() {
-
+            return this.color[0].length;
         }
     }
 
