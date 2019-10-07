86ce8801653a013316a5f6de99544dfdfd6ca383
Meg Dahlgren <meg.dahlgren@gmail.com>
2019-10-08 12:39:40 +1300

    Add inner class to handle baseball data

--- baseball/BaseballElimination.java
+++ baseball/BaseballElimination.java
@@ -11,70 +11,36 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class BaseballElimination {
-
-    private HashMap<Integer, int[]> remainingGames = new HashMap<>();
-    private HashMap<Integer, int[]> currentRecord = new HashMap<>();
-    private String[] teamNumToName;
-    private int numTeams;
+    private BaseballData divisionData;
 
     // create a baseball division from given filename in format specified below
     public BaseballElimination(String filename) {
-        In in = new In(filename);
-        this.numTeams = Integer.parseInt(in.readLine());
-        this.teamNumToName = new String[this.numTeams];
-        int teamNumber = 0;
-        while (!in.isEmpty()) {
-            String[] line = in.readLine().split("\\s+");
-            String name = line[0];
-            int[] remainingLineup = new int[this.numTeams];
-            int[] record = new int[3];
-            teamNumToName[teamNumber] = name;
-            record[0] = Integer.parseInt(line[1]);
-            record[1] = Integer.parseInt(line[2]);
-            record[2] = Integer.parseInt(line[3]);
-            int next = 4;
-            for (int i = 0; i < this.numTeams; i++) {
-                remainingLineup[i] = Integer.parseInt(line[next]);
-                next++;
-            }
-            this.remainingGames.put(teamNumber, remainingLineup);
-            this.currentRecord.put(teamNumber, record);
-            teamNumber++;
-        }
+        this.divisionData = new BaseballData(filename);
     }
 
-    private void testDataInput() {
-        for (int i = 0; i < this.numTeams; i++) {
+    private void printTeamsData() {
+        int numTeams = this.divisionData.numTeams;
+        for (int i = 0; i < numTeams; i++) {
             StdOut.println();
-            StdOut.println("team name: " + this.teamNumToName[i]);
-            int[] record = currentRecord.get(i);
-            int[] lineup = remainingGames.get(i);
-            StdOut.println(" wins: " + record[0] + " losses: " + record[1] + " remaining games: "
-                                   + record[2]);
-            StdOut.println(" remaining games against: ");
-            for (int j = 0; j < lineup.length; j++) {
-                StdOut.print("  " + this.teamNumToName[j] + ": " + lineup[j]);
+            TeamData data = this.divisionData.teamData.get(i);
+            StdOut.println("team name " + data.name());
+            StdOut.println(
+                    " wins: " + data.wins() + " losses: " + data.losses() + " remaining games: "
+                            + data.remaining());
+            StdOut.println("remaining games against: ");
+            for (int j = 0; j < numTeams; j++) {
+                StdOut.print(
+                        "  " + this.divisionData.allTeams.get(j) + ": " + data.gamesAgainst(j));
             }
             StdOut.println();
         }
-        StdOut.println("all teams:");
-        for (String team : teams()) StdOut.print(team + " ");
-        StdOut.println();
     }
 
     // number of teams
-    public int numberOfTeams() {
-        return this.numTeams;
-    }
+    // public int numberOfTeams() {}
 
     // all teams
-    public Iterable<String> teams() {
-        ArrayList<String> teams = new ArrayList<>();
-        for (int i = 0; i < this.numTeams; i++) {
-            teams.add(this.teamNumToName[i]);
-        }
-        return teams;
-    }
+    // public Iterable<String> teams() {}
 
     // number of wins for given team
     // public int wins(String team) {}
@@ -96,7 +62,7 @@ public class BaseballElimination {
 
     public static void main(String[] args) {
         BaseballElimination division = new BaseballElimination(args[0]);
-        division.testDataInput();
+        division.printTeamsData();
         // for (String team : division.teams()) {
         //     if (division.isEliminated(team)) {
         //         StdOut.print(team + " is eliminated by the subset R = { ");
@@ -109,5 +75,71 @@ public class BaseballElimination {
         //         StdOut.println(team + " is not eliminated");
         //     }
         // }
+    }
+
+    private class BaseballData {
+        private int numTeams;
+        private ArrayList<String> allTeams = new ArrayList<>();
+        private HashMap<Integer, TeamData> teamData = new HashMap<>();
+
+        public BaseballData(String filename) {
+            In in = new In(filename);
+            this.numTeams = Integer.parseInt(in.readLine());
+            int teamNumber = 0;
+            while (!in.isEmpty()) {
+                String[] line = in.readLine().split("\\s+");
+                this.allTeams.add(line[0]);
+                this.teamData.put(teamNumber, new TeamData(teamNumber, line, this.numTeams));
+                teamNumber++;
+            }
+        }
+
+
+    }
+
+    private class TeamData {
+        private String name;
+        private int number;
+        private int wins;
+        private int losses;
+        private int remaining;
+        private HashMap<Integer, Integer> remainingAgainst = new HashMap<>();
+
+        public TeamData(int number, String[] line, int numTeams) {
+            this.name = line[0];
+            this.number = number;
+            this.wins = Integer.parseInt(line[1]);
+            this.losses = Integer.parseInt(line[2]);
+            this.remaining = Integer.parseInt(line[3]);
+            int next = 4;
+            for (int i = 0; i < numTeams; i++) {
+                remainingAgainst.put(i, Integer.parseInt(line[next]));
+                next++;
+            }
+        }
+
+        public int number() {
+            return this.number;
+        }
+
+        public String name() {
+            return this.name;
+        }
+
+        public int wins() {
+            return this.wins;
+        }
+
+        public int losses() {
+            return this.losses;
+        }
+
+        public int remaining() {
+            return this.remaining;
+        }
+
+        public int gamesAgainst(int teamNumber) {
+            return this.remainingAgainst.get(teamNumber);
+        }
     }
 }
