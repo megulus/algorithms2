72197d96b5df50f075c0246e83ae4e6497aac8fd
Meg Dahlgren <meg.dahlgren@gmail.com>
2019-11-08 11:40:56 -0500

    Add private helper classes - Vertex, Team, Division

--- baseball/BaseballElimination.java
+++ baseball/BaseballElimination.java
@@ -4,6 +4,7 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.FlowNetwork;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
@@ -11,28 +12,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class BaseballElimination {
-    private BaseballData divisionData;
+    private DivisionData divisionData;
+    private FlowNetwork flowNetwork;
 
     // create a baseball division from given filename in format specified below
     public BaseballElimination(String filename) {
-        this.divisionData = new BaseballData(filename);
-    }
-
-    private void printTeamsData() {
-        StdOut.println("number of teams: " + numberOfTeams());
-        for (String team : this.divisionData.allTeams()) {
-            StdOut.println("team name: " + team + " wins: " + wins(team));
-            // TeamData data = this.divisionData.teamData.get(i);
-            // StdOut.println("team name " + data.name());
-            // StdOut.println(
-            //         " wins: " + data.wins() + " losses: " + data.losses() + " remaining games: "
-            //                 + data.remaining());
-            // StdOut.println("remaining games against: ");
-            // for (int j = 0; j < numTeams; j++) {
-            //     StdOut.print(
-            //             "  " + this.divisionData.allTeams.get(j) + ": " + data.gamesAgainst(j));
-            // }
-        }
+        this.divisionData = new DivisionData(filename);
+        this.flowNetwork = new FlowNetwork((this.divisionData.numberOfTeams() * 2) + 2);
     }
 
     // number of teams
@@ -66,8 +52,13 @@ public class BaseballElimination {
     // public Iterable<String> certificateOfElimination(String team) {}
 
     public static void main(String[] args) {
-        BaseballElimination division = new BaseballElimination(args[0]);
-        division.printTeamsData();
+        BaseballElimination be = new BaseballElimination(args[0]);
+        int numberOfTeams = be.numberOfTeams();
+        StdOut.println("numberOfTeams: " + numberOfTeams);
+        StdOut.println("running DivisionData class printDivisionData() method:");
+        be.divisionData.printDivisionData();
+
+
         // for (String team : division.teams()) {
         //     if (division.isEliminated(team)) {
         //         StdOut.print(team + " is eliminated by the subset R = { ");
@@ -82,19 +73,78 @@ public class BaseballElimination {
         // }
     }
 
-    private class BaseballData {
+    enum Type {
+        START, GAME, TEAM, END
+    }
+
+    private class Vertex {
+        int number;
+        Type type;
+        Team team1 = null;
+        Team team2 = null;
+
+        public Vertex(int number, Type type, Team team1, Team team2) {
+            this.number = number;
+            this.type = type;
+            if (team1 != null) this.team1 = team1;
+            if (team2 != null) this.team2 = team2;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == this) return true;
+            if (!(o instanceof Vertex)) return false;
+
+            Vertex v = (Vertex) o;
+
+            return v.type.equals(this.type) && v.team1.equals(this.team1) && v.team2
+                    .equals(this.team2);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = 17;
+            result = 31 * (result + this.type.hashCode());
+            result = 31 * (result + this.team1.hashCode());
+            result = 31 * (result + this.team2.hashCode());
+            return result;
+        }
+
+    }
+
+    private class VertexTracker {
+        HashMap<Vertex, Integer> VtoI;
+        HashMap<Integer, Vertex> ItoV;
+        int n;
+        int totalVertices;
+
+        public VertexTracker(int numberOfTeams) {
+            this.n = numberOfTeams;
+            this.totalVertices = getTotalVertices();
+        }
+
+        public int getTotalVertices() {
+            return (((this.n - 1) * (this.n - 2)) / 2) + (this.n - 1);
+        }
+
+        // translate from vertex to integer
+
+        // translate from integer to vertex
+    }
+
+    private class DivisionData {
         private int numTeams;
         private ArrayList<String> allTeams = new ArrayList<>();
-        private HashMap<Integer, TeamData> teamData = new HashMap<>();
+        private HashMap<Integer, Team> numberToTeamMap = new HashMap<>();
 
-        public BaseballData(String filename) {
+        public DivisionData(String filename) {
             In in = new In(filename);
             this.numTeams = Integer.parseInt(in.readLine());
             int teamNumber = 0;
             while (!in.isEmpty()) {
                 String[] line = in.readLine().split("\\s+");
                 this.allTeams.add(line[0]);
-                this.teamData.put(teamNumber, new TeamData(teamNumber, line, this.numTeams));
+                this.numberToTeamMap.put(teamNumber, new Team(teamNumber, line, this.numTeams));
                 teamNumber++;
             }
         }
@@ -108,13 +158,28 @@ public class BaseballElimination {
         }
 
         public int wins(String name) {
-            TeamData data = teamData.get(this.allTeams.indexOf(name));
-            return data.wins();
+            Team team = numberToTeamMap.get(this.allTeams.indexOf(name));
+            return team.wins();
+        }
+
+        private void printDivisionData() {
+            for (int i = 0; i < this.allTeams.size(); i++) {
+                Team team = this.numberToTeamMap.get(i);
+                StdOut.println(
+                        " team name: " + team.name() + " wins: " + team.wins() + " losses: " + team
+                                .losses());
+                StdOut.println("  games remaining against: ");
+                for (int j = 0; j < this.allTeams.size(); j++) {
+                    String opponent = this.allTeams.get(j);
+                    StdOut.println("   " + opponent + ": " + team.gamesAgainst(j));
+                }
+                StdOut.println();
+            }
         }
 
     }
 
-    private class TeamData {
+    private class Team {
         private String name;
         private int number;
         private int wins;
@@ -122,7 +187,7 @@ public class BaseballElimination {
         private int remaining;
         private HashMap<Integer, Integer> remainingAgainst = new HashMap<>();
 
-        public TeamData(int number, String[] line, int numTeams) {
+        public Team(int number, String[] line, int numTeams) {
             this.name = line[0];
             this.number = number;
             this.wins = Integer.parseInt(line[1]);
@@ -135,7 +200,7 @@ public class BaseballElimination {
             }
         }
 
-        public int numberTeams() {
+        public int number() {
             return this.number;
         }
 
